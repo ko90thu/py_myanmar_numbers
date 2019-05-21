196de1d831ace72198545c7ac126704cdb6d81a2
MyatThu <myat.thu@frontiir.net>
2019-05-21 09:19:59 +0630

    [REMOVE] remove decode utf-8

--- converter.py
+++ converter.py
@@ -6,7 +6,7 @@ def convert(input_numbers):
         convert('၁၂၃၄၅၇')
     '''
     mm_numbers = [u'\u1040',u'\u1041',u'\u1042',u'\u1043',u'\u1044',u'\u1045',u'\u1046',u'\u1047',u'\u1048',u'\u1049']
-    lst_input_numbers = list(str(input_numbers).decode('utf8'))
+    lst_input_numbers = list(str(input_numbers))
     result = int(''.join([str(mm_numbers.index(inn)) for inn in lst_input_numbers]))
     return result
 
